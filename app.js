417f865bc801108a13e0389843fcbf1d472d33ed
Valdsj <valentin.de-saint-jacob@epitech.eu>
2020-09-20 19:21:09 +0200

    MOD: about page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, '-=1.5')
     id: 'home'
   }, 'Home', '/'));
 
- const homeBtn = document.querySelector('#btn-home')
+ const homeBtn = document.querySelector('#btn-home');
 homeBtn.addEventListener('click', function(event) {
   tl.fromTo('.about', {x: '0%', duration: 1.5}, {x: '100%', duration: 1.5});
   tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: .5});
@@ -18,13 +18,23 @@ homeBtn.addEventListener('click', function(event) {
   }, 'Home', '/');
 });
 
-const aboutBtn = document.querySelector('#btn-about')
+const aboutBtn = document.querySelector('#btn-about');
 aboutBtn.addEventListener('click', function(event) {
   tl.fromTo('.about', {x: '100%', duration: 1.5}, {x: 0, y: 0, duration: 1.5});
   tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: .5});
   history.pushState({
     id: 'about'
   }, 'About', '/about');
+});
+
+const skillsIcon = document.querySelectorAll('.skills img');
+skillsIcon.forEach(function(elem, i) {
+  elem.addEventListener('mouseenter', function(event) {
+    tl.to(elem, {css: {scale: 2.5}, duration: .25}, '-=.5');
+  });
+  elem.addEventListener('mouseleave', function(event) {
+    tl.to(elem, {css: {scale: 1}, duration: .25});
+  });
 });
 
 window.addEventListener('popstate', function (event) {
